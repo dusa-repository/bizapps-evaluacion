82ff3bfaee7939469330abb92a6d0195bddb6239
linkin9014@gmail.com <linkin9014@gmail.com@e219fbfa-0495-b595-24c1-54f2a0e40c93>
2014-08-07 14:49:38 +0000

    Arbolesa admiten n niveles...

--- Bizapps-evaluacion/src/controlador/transacciones/CEmpleado.java
+++ Bizapps-evaluacion/src/controlador/transacciones/CEmpleado.java
@@ -118,13 +118,7 @@ public class CEmpleado extends CGenerico {
 	 * crear un arbol estructurado, segun la distribucion de las mismas
 	 */
 	private Nodos getFooRoot() {
-
 		Nodos root = new Nodos(null, 0, "", "");
-		Nodos oneLevelNode = new Nodos(null, 0, "", "");
-		Nodos twoLevelNode = new Nodos(null, 0, "", "");
-		Nodos threeLevelNode = new Nodos(null, 0, "", "");
-		Nodos fourLevelNode = new Nodos(null, 0, "", "");
-
 		Authentication auth = SecurityContextHolder.getContext()
 				.getAuthentication();
 		Usuario u = servicioUsuario.buscarUsuarioPorNombre(auth.getName());
@@ -133,99 +127,57 @@ public class CEmpleado extends CGenerico {
 				.getCedula());
 		List<Empleado> arboles = new ArrayList<Empleado>();
 		ArrayList<Integer> ids = new ArrayList<Integer>();
+		List<Empleado> hijos = new ArrayList<Empleado>();
 		for (int k = 0; k < empleado.size(); k++) {
-			ids.add(empleado.get(k).getId());
-
+			ids.add(Integer.valueOf(empleado.get(k).getFicha()));
+			hijos = servicioEmpleado.BuscarPorSupervisor(empleado.get(k)
+					.getFicha());
+			for (int i = 0; i < hijos.size(); i++) {
+				empleado.add(hijos.get(i));
+			}
 		}
-
-		Collections.sort(ids);
 		Empleado jefe = servicioEmpleado.buscarPorFicha(u.getCedula());
 		arboles.add(jefe);
 		for (int t = 0; t < ids.size(); t++) {
 			Empleado a;
-			a = servicioEmpleado.buscarPorId(ids.get(t));
+			a = servicioEmpleado.buscarPorFicha(String.valueOf(ids.get(t)));
 			arboles.add(a);
 		}
-		String temp1, temp2, temp3 = "";
-		for (int i = 0; i < arboles.size(); i++) {
-			String ficha = jefe.getFicha();
-			if (arboles.get(i).getFicha().equals(ficha)) {
-				oneLevelNode = new Nodos(root, i, "("
-						+ arboles.get(i).getGradoAuxiliar() + ")" + " "
-						+ arboles.get(i).getNombre(), arboles.get(i).getFicha());
-				// oneLevelNode = new Nodos(root, i, "("
-				// + arboles.get(i).getGradoAuxiliar() + ")" + "  "
-				// + arboles.get(i).getFicha());
-				root.appendChild(oneLevelNode);
-				temp1 = arboles.get(i).getFicha();
-				arboles.remove(i);
-				List<Empleado> hijos = servicioEmpleado
-						.BuscarPorSupervisor(temp1);
-				for (int j = i; j < hijos.size(); j++) {
-					if (temp1.equals(hijos.get(j).getFichaSupervisor())) {
+		List<Nodos> nodos = new ArrayList<Nodos>();
+		List<String> idsPadre = new ArrayList<String>();
+		return crearArbol(root, nodos, arboles, 0, idsPadre, jefe.getFicha());
+	}
 
-						twoLevelNode = new Nodos(root, i, "("
-								+ hijos.get(i).getGradoAuxiliar() + ")" + " "
-								+ hijos.get(i).getNombre(), hijos.get(i)
+	private Nodos crearArbol(Nodos roote, List<Nodos> nodos,
+			List<Empleado> arboles, int i, List<String> idsPadre, String cedula) {
+		System.out.println("arboles" + arboles.size());
+		for (int z = 0; z < arboles.size(); z++) {
+			Nodos oneLevelNode = new Nodos(null, 0, "", "");
+			Nodos two = new Nodos(null, 0, "", "");
+			if (arboles.get(z).getFicha().equals(cedula)) {
+				oneLevelNode = new Nodos(roote, z, "("
+						+ arboles.get(z).getGradoAuxiliar() + ")" + " "
+						+ arboles.get(z).getNombre(), arboles.get(z).getFicha());
+				roote.appendChild(oneLevelNode);
+				idsPadre.add(arboles.get(z).getFicha());
+				nodos.add(oneLevelNode);
+			} else {
+				for (int j = 0; j < idsPadre.size(); j++) {
+					if (idsPadre.get(j).equals(
+							arboles.get(z).getFichaSupervisor())) {
+						oneLevelNode = nodos.get(j);
+						two = new Nodos(oneLevelNode, z, "("
+								+ arboles.get(z).getGradoAuxiliar() + ")" + " "
+								+ arboles.get(z).getNombre(), arboles.get(z)
 								.getFicha());
-						// new Nodos(oneLevelNode, i, "("
-						// + hijos.get(i).getGradoAuxiliar() + ")" + "  "
-						// + hijos.get(j).getNombre());
-						oneLevelNode.appendChild(twoLevelNode);
-						temp2 = hijos.get(j).getFicha();
-						hijos.remove(j);
-						List<Empleado> hijos2 = servicioEmpleado
-								.BuscarPorSupervisor(temp2);
-						for (int k = j; k < hijos2.size(); k++) {
-
-							if (temp2
-									.equals(hijos2.get(k).getFichaSupervisor())) {
-
-								threeLevelNode = new Nodos(root, i,
-										"(" + hijos2.get(i).getGradoAuxiliar()
-												+ ")" + " "
-												+ hijos2.get(i).getNombre(),
-										hijos2.get(i).getFicha());
-								// new Nodos(twoLevelNode, i, "("
-								// + hijos2.get(i).getGradoAuxiliar()
-								// + ")" + "  "
-								// + hijos2.get(k).getNombre());
-								twoLevelNode.appendChild(threeLevelNode);
-								temp3 = hijos2.get(k).getFicha();
-								hijos2.remove(k);
-								List<Empleado> hijos3 = servicioEmpleado
-										.BuscarPorSupervisor(temp3);
-								for (int z = k; z < hijos3.size(); z++) {
-
-									if (temp3.equals(hijos3.get(z)
-											.getFichaSupervisor())) {
-										fourLevelNode = new Nodos(root, i, "("
-												+ hijos3.get(i)
-														.getGradoAuxiliar()
-												+ ")" + " "
-												+ hijos3.get(i).getNombre(),
-												hijos3.get(i).getFicha());
-
-										// new Nodos(
-										// threeLevelNode, i, hijos3
-										// .get(z).getNombre());
-										threeLevelNode
-												.appendChild(fourLevelNode);
-										hijos3.remove(z);
-
-										z = z - 1;
-									}
-								}
-								k = k - 1;
-							}
-						}
-						j = j - 1;
+						oneLevelNode.appendChild(two);
+						idsPadre.add(arboles.get(z).getFicha());
+						nodos.add(two);
 					}
 				}
-				i = i - 1;
 			}
 		}
-		return root;
+		return roote;
 	}
 
 	@Listen("onClick = #arbolPersonal")
